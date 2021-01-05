67d11248360877cb17e61ffc409f058294b16921
ProL22 <65721809+ProL22@users.noreply.github.com>
2021-01-05 20:15:15 +0530

    Add files via upload

---
+++ Screens/Read.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {StyleSheet,Text,View,Image} from 'react-native';
+
+export default class Read extends React.Component{
+  render(){
+    return(
+           <Text>"Welcome Back"</Text>
+    )
+  }
+}
---
+++ Screens/Write.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {StyleSheet,Text,Image,View} from 'react-native';
+
+export default class Write extends React.Component{
+  render(){
+    return(
+      <Text>"Welcome"</Text>
+    )
+  }
+}
